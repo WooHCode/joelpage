39d84ff6c0463af9e4014dd4fa362c446d36569c
WooHCode <gusdn526@naver.com>
2022-12-22 19:58:14 +0900

    initial commit

---
+++ src/main/java/joel/joelpage/JoelpageApplication.java
@@ -0,0 +1,13 @@
+package joel.joelpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoelpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoelpageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/joel/joelpage/JoelpageApplicationTests.java
@@ -0,0 +1,13 @@
+package joel.joelpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoelpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
