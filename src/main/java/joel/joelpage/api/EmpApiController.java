d0cd3632542b247712fdeb73a579ab33ff6a1c18
WooHCode <gusdn526@naver.com>
2023-01-25 01:27:58 +0900

    emp api crud 간단 구현

--- src/main/java/joel/joelpage/api/EmpApiController.java
+++ src/main/java/joel/joelpage/api/EmpApiController.java
@@ -1,13 +1,13 @@
 package joel.joelpage.api;
 
+import joel.joelpage.dto.UpdateEmployeeDto;
+import joel.joelpage.dto.UpdateItemDto;
 import joel.joelpage.entity.Employee;
 import joel.joelpage.service.EmployeeService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,11 +30,33 @@ public class EmpApiController {
         return new CreateEmpResponse(id);
     }
 
+    @PatchMapping("/api/v1/emp/update/{empId}")
+    public HttpStatus updateEmp(@PathVariable("empId") Long id, @RequestBody UpdateEmployeeDto dto) {
+        Employee findEmp = employeeService.findOneEmp(id);
+        findEmp.toEntity(dto.getEmpName(),dto.getEmpGender(), dto.getEmpWorkCount(), dto.getEmpPay(), dto.getEmpAge(),dto.getEmpDescription());
+        return HttpStatus.OK;
+    }
+
+    @DeleteMapping("/api/v1/emp/delete/{empId}")
+    public HttpStatus deleteEmp(@PathVariable("empId") Long id) {
+        employeeService.deleteEmp(id);
+        return HttpStatus.OK;
+    }
+
     @Data
     static class CreateEmpResponse {
         public Long id;
 
         public CreateEmpResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+    @Data
+    static class DeleteEmpResponse {
+        public Long id;
+
+        public DeleteEmpResponse(Long id) {
             this.id = id;
         }
     }
