98e90161cf8d07687e76efc1c46ee897689d215b
WooHCode <gusdn526@naver.com>
2023-02-18 10:09:50 +0900

    update entity sales

--- src/main/java/joel/joelpage/entity/Sale.java
+++ src/main/java/joel/joelpage/entity/Sale.java
@@ -5,8 +5,9 @@ import java.time.LocalDate;
 //Many to one
 public class Sale {
     private Long id;
+    private String itemName;
+    private String itemPrice;
     private LocalDate saleDate;
-    private int totalSales;
     private int saleCount;
     private int itemTotalSale;
 }
