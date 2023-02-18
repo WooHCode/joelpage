d30381ebc4508f34eac86fe51974f1dbc8de97e4
WooHCode <gusdn526@naver.com>
2023-02-19 01:00:02 +0900

    update entity sales

--- src/main/java/joel/joelpage/entity/Sale.java
+++ src/main/java/joel/joelpage/entity/Sale.java
@@ -1,13 +1,50 @@
 package joel.joelpage.entity;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
-//Many to one
+@Getter
+@Entity
+@NoArgsConstructor
 public class Sale {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
-    private String itemName;
-    private String itemPrice;
+
+    @Column(name = "s_item_name")
+    private String saleItemName;
+
+    @Column(name = "s_item_price")
+    private String saleItemPrice;
+
+    @Column(name = "sale_date")
     private LocalDate saleDate;
+
+    @Column(name = "sale_count")
     private int saleCount;
+
+    @Column(name = "item_total_sale")
     private int itemTotalSale;
+
+    @Builder
+    public Sale(Long id, String saleItemName, String saleItemPrice, LocalDate saleDate, int saleCount, int itemTotalSale) {
+        this.id = id;
+        this.saleItemName = saleItemName;
+        this.saleItemPrice = saleItemPrice;
+        this.saleDate = saleDate;
+        this.saleCount = saleCount;
+        this.itemTotalSale = itemTotalSale;
+    }
+    public void toEntity(String saleItemName, String saleItemPrice, LocalDate saleDate, int saleCount, int itemTotalSale) {
+        this.saleItemName = saleItemName;
+        this.saleItemPrice = saleItemPrice;
+        this.saleDate = saleDate;
+        this.saleCount = saleCount;
+        this.itemTotalSale = itemTotalSale;
+    }
 }
