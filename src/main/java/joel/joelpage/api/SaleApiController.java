88133a0493ec8d327421509df5f1b05615146c72
WooHCode <gusdn526@naver.com>
2023-02-21 22:15:11 +0900

    feature saleApiController

---
+++ src/main/java/joel/joelpage/api/SaleApiController.java
@@ -0,0 +1,27 @@
+package joel.joelpage.api;
+
+import joel.joelpage.dto.SaleDto;
+import joel.joelpage.entity.Sale;
+import joel.joelpage.service.SaleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class SaleApiController {
+    private final SaleService saleService;
+
+    @GetMapping("/api/v1/sale")
+    public List<SaleDto> getAllSales() {
+        List<Sale> allSales = saleService.findAllSales();
+        List<SaleDto> saleDtos = allSales.stream().map(s ->
+                new SaleDto(s.getId(), s.getSaleItemName(), s.getSaleItemPrice(), s.getSaleDate(),
+                        s.getSaleItemCode(), s.getSaleCount(), s.getItemTotalSale())).collect(Collectors.toList());
+        return saleDtos;
+    }
+}
