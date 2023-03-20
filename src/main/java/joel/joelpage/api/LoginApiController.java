af3f0353a7cc4059a9f2b9a73d22b98a1f4da8b1
WooHCode <gusdn526@naver.com>
2023-03-21 01:03:50 +0900

    update 로그인기능

--- src/main/java/joel/joelpage/api/LoginApiController.java
+++ src/main/java/joel/joelpage/api/LoginApiController.java
@@ -1,4 +1,27 @@
 package joel.joelpage.api;
 
+import joel.joelpage.entity.LoginMember;
+import joel.joelpage.repository.LoginRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
 public class LoginApiController {
+
+    private final LoginRepository loginRepository;
+
+    @PostMapping("/api/account/login")
+    public Long login(@RequestBody Map<String, String> params) {
+        LoginMember loginMember = loginRepository.findByMemberIdAndPassword(params.get("ide"), params.get("password"));
+
+        if (loginMember != null) {
+            return loginMember.getSeq();
+        }
+        return 0l;
+    }
 }
