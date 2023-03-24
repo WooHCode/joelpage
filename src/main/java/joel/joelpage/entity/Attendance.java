4ea6261a2b31013f52614b57e4673dfb8529194e
WooHCode <gusdn526@naver.com>
2023-03-24 23:04:59 +0900

    직원출근페이지 개발 시작

--- src/main/java/joel/joelpage/entity/Attendance.java
+++ src/main/java/joel/joelpage/entity/Attendance.java
@@ -1,6 +1,7 @@
 package joel.joelpage.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Attendance {
 
     @Id
@@ -16,16 +18,10 @@ public class Attendance {
     @Column(name = "att_id", nullable = false)
     private Long id;
 
-    @Column(name = "date_time", nullable = false)
+    @Column(name = "att_time", nullable = false)
     private LocalDateTime attendTime;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "employee_id")
     private Employee employee;
-
-    public Attendance(Long id, LocalDateTime attendTime, Employee employee) {
-        this.id = id;
-        this.attendTime = attendTime;
-        this.employee = employee;
-    }
 }
