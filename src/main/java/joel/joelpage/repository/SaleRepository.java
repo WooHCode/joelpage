6854ccd6a469e56d912e79660e5cd013fc4e544e
WooHCode <gusdn526@naver.com>
2023-03-24 19:28:46 +0900

    매출문제 해결(오늘 데이터가 00시기준이어서 localdatetime으로 Repository변경)

--- src/main/java/joel/joelpage/repository/SaleRepository.java
+++ src/main/java/joel/joelpage/repository/SaleRepository.java
@@ -7,14 +7,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
     List<Sale> findBySaleItemCode(ItemCode itemCode);
-    List<Sale> findBySaleDateBetween(LocalDate startDate, LocalDate endDate);
+    List<Sale> findBySaleDateBetween(LocalDateTime startDate, LocalDateTime endDate);
 
     @Query("SELECT s FROM Sale s WHERE s.saleDate >= :startDate AND s.saleDate < :endDate")
-    List<Sale> findAllBySaleDateBetween(LocalDate startDate, LocalDate endDate);
+    List<Sale> findAllBySaleDateBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
