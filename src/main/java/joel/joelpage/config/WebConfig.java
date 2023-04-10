b96132fad3d65aaa243182dfd00b1c5a36aa839d
WooHCode <gusdn526@naver.com>
2023-04-10 21:43:31 +0900

    cors error 중복 Configuration추가

---
+++ src/main/java/joel/joelpage/config/WebConfig.java
@@ -0,0 +1,16 @@
+package joel.joelpage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/empDetail/**")
+                .allowedOrigins("http://joeladmin.store", "http://ec2-52-79-168-230.ap-northeast-2.compute.amazonaws.com:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
+
