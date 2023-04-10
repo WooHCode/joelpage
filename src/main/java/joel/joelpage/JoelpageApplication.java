36b2e162e82b109c16e13bdf38c85ebd8f7c1462
WooHCode <gusdn526@naver.com>
2023-04-10 20:41:44 +0900

    ci/cd pipeline

--- src/main/java/joel/joelpage/JoelpageApplication.java
+++ src/main/java/joel/joelpage/JoelpageApplication.java
@@ -2,6 +2,7 @@ package joel.joelpage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
@@ -11,7 +12,9 @@ import org.springframework.web.filter.HiddenHttpMethodFilter;
 public class JoelpageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JoelpageApplication.class, args);
+		SpringApplication application = new SpringApplication(JoelpageApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 
 	@Bean
