0a4298c604046363a727bc543aeb6a3c59827825
WooHCode <gusdn526@naver.com>
2023-04-10 23:19:32 +0900

    경로수정

--- src/main/java/joel/joelpage/config/WebConfig.java
+++ src/main/java/joel/joelpage/config/WebConfig.java
@@ -9,12 +9,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/empDetail/**")
-                .allowedOrigins("http://joeladmin.store", "http://ec2-52-79-168-230.ap-northeast-2.compute.amazonaws.com:8080")
+                .allowedOrigins("http://joeladmin.store", "http://ec2-52-79-168-230.ap-northeast-2.compute.amazonaws.com:8000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
         registry.addMapping("/api/**")
-                .allowedOrigins("http://joeladmin.store", "http://ec2-52-79-168-230.ap-northeast-2.compute.amazonaws.com:8080")
+                .allowedOrigins("http://joeladmin.store", "http://ec2-52-79-168-230.ap-northeast-2.compute.amazonaws.com:8000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
